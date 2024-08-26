d4121e61a344c6786519e2508283a1cc710f49b4
Bueno Nepomuceno <bueno.nepomuceno@comeialabs.com.br>
2024-08-26 14:02:27 -0300

    removed top-level await

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const apiUrl = import.meta.env.VITE_API_URL;
 
-const token = await localStorage.getItem("token");
+const token = localStorage.getItem("token");
 
 const api = axios.create({
   baseURL: apiUrl,
